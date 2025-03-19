efd4f4e0b890873939f758e395a6fd25b262574c
Maël Gruand <maelgruand1@yahoo.com>
2025-03-19 13:54:57 +0100

    creation de .icon et PWA

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'js-editor-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/icons/th.ico',
+  '/icons/th.jpeg',
+];
+
+// Installation du service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepter les requêtes pour les servir depuis le cache si possible
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
